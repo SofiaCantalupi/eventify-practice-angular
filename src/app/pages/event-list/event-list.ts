65df9229400b7d6e70d991a4fc804ec62274478b
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-10-31 15:50:30 -0300

    create event-list page

--- src/app/pages/event-list/event-list.ts
+++ src/app/pages/event-list/event-list.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { EventService } from '../../services/event-service';
 
 @Component({
   selector: 'app-event-list',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './event-list.css',
 })
 export class EventList {
+  eventService = inject(EventService);
 
+  // se guarda la referencia a la signal del servicio
+  events = this.eventService.events;
+
+  ngOnInit(): void {
+    // refresca la lista cada vez que se ingresa a la pagina, como getEvents hace la peticion get y events es una referencia al signal del servicio, se actualiza
+    this.eventService.getEvents();
+  }
 }
