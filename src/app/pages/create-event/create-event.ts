de7f5e86b4f43c9c1c7c344a03f18d77a6cf15f8
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-10-31 15:50:04 -0300

    create create-form page

---
+++ src/app/pages/create-event/create-event.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Footer } from '../../components/footer/footer';
+import { Header } from '../../components/header/header';
+import { EventForm } from '../../components/event-form/event-form';
+
+@Component({
+  selector: 'app-create-event',
+  imports: [Header, Footer, EventForm],
+  templateUrl: './create-event.html',
+  styleUrl: './create-event.css',
+})
+export class CreateEvent {
+
+}
