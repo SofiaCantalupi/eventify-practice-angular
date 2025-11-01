3beeb0a060031d127d50623718d799805c9ef2cd
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-11-01 19:55:27 -0300

    create form in event-form component

--- src/app/components/event-form/event-form.ts
+++ src/app/components/event-form/event-form.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal} from '@angular/core';
+import { Category } from '../../category';
+import { CategoryService } from '../../services/category-service';
+import { EventService } from '../../services/event-service';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-event-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './event-form.html',
   styleUrl: './event-form.css',
 })
-export class EventForm {
+export class EventForm implements OnInit {
+  categoryService = inject(CategoryService);
+  eventService = inject(EventService);
+  fb = inject(FormBuilder);
 
+  categories = this.categoryService.categories;
+
+  eventForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    category: ['', Validators.required],
+    date: ['', Validators.required],
+    description: ['', [Validators.required, Validators.minLength(3)]],
+  });
+
+  ngOnInit(): void {
+    this.categoryService.getCategories();
+  }
+
+  onSubmit() {
+    if (this.eventForm.valid) {
+    const eventData = {
+      name: this.eventForm.value.name!,
+      category: this.eventForm.value.category!,
+      date: this.eventForm.value.date!,
+      description: this.eventForm.value.description!
+    };
+
+    console.log(eventData);
+    this.eventService.createEvent(eventData);
+  
+  }
+}
 }
