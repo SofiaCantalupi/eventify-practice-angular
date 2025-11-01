f6e483f330b6e683ad823a333c378ed110a4966a
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-11-01 19:56:56 -0300

    add createEvent method in event-service

---
+++ src/app/services/event-service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { signal } from '@angular/core';
+import { EventI } from '../eventI';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EventService {
+  private readonly urlApi = 'http://localhost:3000/events';
+
+  private eventsState = signal<EventI[]>([]);
+
+  public events = this.eventsState.asReadonly();
+
+  constructor(private http: HttpClient) {
+    this.getEvents();
+  }
+
+  getEvents() {
+    this.http.get<EventI[]>(this.urlApi).subscribe({
+      next: (data) => {
+        this.eventsState.set(data);
+      },
+      error: (error) => {
+        console.error('Error loading the events:', error);
+      },
+    });
+  }
+
+  createEvent(event: Omit<EventI, 'id'>){
+    this.http.post<EventI>(this.urlApi, event).subscribe({
+      next: (data) => {
+        this.eventsState.update(events => [...events, data]);
+      },
+      error: (error) => {
+        console.log('Error creating the event:', event)
+      }
+    });
+  }
+}
