ddcfbf5d9f17f435a0ee1c0e758b7ce41d6a382f
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-11-01 23:38:37 -0300

    add helper method in eventservice

--- src/app/pages/event-list/event-list.ts
+++ src/app/pages/event-list/event-list.ts
@@ -1,5 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { EventService } from '../../services/event-service';
+import { CategoryService } from '../../services/category-service';
+import { Category } from '../../category';
 
 @Component({
   selector: 'app-event-list',
@@ -9,12 +11,19 @@ import { EventService } from '../../services/event-service';
 })
 export class EventList {
   eventService = inject(EventService);
+  categoryService = inject(CategoryService);
 
   // se guarda la referencia a la signal del servicio
   events = this.eventService.events;
+  categories = this.categoryService.categories;
 
   ngOnInit(): void {
     // refresca la lista cada vez que se ingresa a la pagina, como getEvents hace la peticion get y events es una referencia al signal del servicio, se actualiza
     this.eventService.getEvents();
+  }
+
+  getCategoryName(categoryId: string): string {
+    const category: Category | undefined = this.categories().find((c) => c.id === categoryId);
+    return category ? category.name : 'Sin categoría';
   }
 }
