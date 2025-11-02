8cadfae282655ed0a29303aa74df779c7ff16fcb
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-11-01 23:35:02 -0300

    add path configuration in app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { CreateEvent } from './pages/create-event/create-event';
+import { EventList } from './pages/event-list/event-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'events',
+    pathMatch: 'full' // Cuando se usa path: '' con redirectTo → siempre debe usarse pathMatch: 'full'
+  },
+  {
+    path: 'create',
+    component: CreateEvent,
+  },
+  {
+    path: 'events',
+    component: EventList,
+  },
+  {
+    path: '**',
+    redirectTo: 'events',
+  }
+];
